5bfa6630e2e72be2eecefc94b9d8d5ef106c4958
pooja9522 <billupatel26@gmail.com>
2025-01-31 14:54:14 +0530

    done

--- springboottest/src/main/java/com/rays/validation/ValidLong.java
+++ springboottest/src/main/java/com/rays/validation/ValidLong.java
@@ -1,19 +1,22 @@
 package com.rays.validation;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
 @Constraint(validatedBy = ValidLongValidator.class)
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidLong {
-	String message() default "Invalid input";
+    String message() default "Invalid input";
 
-	Class<?>[] groups() default {};
+    Class<?>[] groups() default {};
 
-	Class<? extends Payload>[] payload() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    boolean allowEmpty() default false; // New attribute to allow empty values
 }
